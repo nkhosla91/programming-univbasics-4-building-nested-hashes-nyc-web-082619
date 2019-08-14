64c9cd77d60290b4b615dd0207427d7f46409a67
Neeraj Khosla <n.khosla91@gmail.com>
2019-08-14 14:35:12 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,17 @@
 def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
 	base_hash = {:railroads => {}	}
 end
 
 def monopoly_with_second_tier
   monopoly_with_second_tier_hash = {
                  :railroads => {:pieces => 4, 
-                                :rent_in_dollars => {
-                                            :one_piece_owned => 25,
-                                            :two_pieces_owned => 50,
-                                            :three_pieces_owned => 100,
-                                            :four_pieces_owned => 200
-                                                    },
-                                 :names => {}
-                                }                          
-                                    }
-
+                                :rent_in_dollars => {}
+                               }
+                                   }
+end
 def monopoly_with_third_tier
    monopoly_with_third_tier_hash = {
-                 :railroads => {:pieces => 4, 
+                                :railroads => {:pieces => 4, 
                                 :rent_in_dollars => {
                                             :one_piece_owned => 25,
                                             :two_pieces_owned => 50,
@@ -32,23 +24,28 @@ def monopoly_with_third_tier
                                             :b_and_o_railroad => {},
                                             :shortline_railroad => {}
                                                     }                          }
-                                    }.
+                                    }
 end
 
 def monopoly_with_fourth_tier
-    monopoly_with_third_tier_hash = {
-                 :railroads => {:pieces => 4, 
-                                :rent_in_dollars => {
-                                            :one_piece_owned => 25,
-                                            :two_pieces_owned => 50,
-                                            :three_pieces_owned => 100,
-                                            :four_pieces_owned => 200
-                                                    },
-                                :names => {
-                                            :reading_railroad => {},
-                                            :pennsylvania_railroad => {},
-                                            :b_and_o_railroad => {},
-                                            :shortline_railroad => {}
-                                                    }                          }
+   monopoly_with_fourth_tier_hash = {
+                          :railroads => {:pieces => 4, 
+                          :rent_in_dollars => {
+                                      :one_piece_owned => 25,
+                                      :two_pieces_owned => 50,
+                                      :three_pieces_owned => 100,
+                                      :four_pieces_owned => 200
+                                               },
+                          :names => {
+                                      :reading_railroad => 
+                                           {"mortgage_value" => "$100"},
+                                      :pennsylvania_railroad => 
+                                           {"mortgage_value" => "$200"},
+                                      :b_and_o_railroad => 
+                                           {"mortgage_value" => "$400"},
+                                      :shortline_railroad => 
+                                           {"mortgage_value" => "$800"}
+                                             }                        
+                                        }
                                     }
-                                  end
+end
